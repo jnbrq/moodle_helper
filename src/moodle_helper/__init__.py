1fff2718161e5aae835a91b9456965bbc88ba2ae
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2021-12-05 15:06:13 +0100

    Bug fixing.

--- src/moodle_helper/__init__.py
+++ src/moodle_helper/__init__.py
@@ -9,3 +9,4 @@ from .quiz_builder import QuizBuilder, QuestionWrapper
 from .moodlexmlhtml import MoodleXMLHTMLRenderer
 from .htmlpreview import HTMLPreview
 from .default_main import default_main
+from .resources import Resources
